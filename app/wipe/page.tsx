bcd7baac75c4d5b9899565b38a88cc026e2ccf9f
geodevcode <geodevinstitute@gmail.com>
2025-07-19 11:34:05 +0100

    wipe page created

---
+++ app/wipe/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { usePuterStore } from "@/lib/puter";
+// import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function WipeApp() {
+  const { auth, isLoading, error, fs, kv } = usePuterStore();
+//   const router = useRouter();
+  const [files, setFiles] = useState<FSItem[]>([]);
+
+  const loadFiles = async () => {
+    const files = (await fs.readDir("./")) as FSItem[];
+    setFiles(files);
+  };
+
+  useEffect(() => {
+    loadFiles();
+  }, []);
+
+//   useEffect(() => {
+//     if (!isLoading && !auth.isAuthenticated) {
+//       router.push("/auth?next=/wipe");
+//     }
+//   }, [isLoading]);
+
+  const handleDelete = async () => {
+    files.forEach(async (file) => {
+      await fs.delete(file.path);
+    });
+    await kv.flush();
+    loadFiles();
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error {error}</div>;
+  }
+
+  return (
+    <div>
+      Authenticated as: {auth.user?.username}
+      <div>Existing files:</div>
+      <div className="flex flex-col gap-4">
+        {files.map((file) => (
+          <div key={file.id} className="flex flex-row gap-4">
+            <p>{file.name}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+          onClick={() => handleDelete()}
+        >
+          Wipe App Data
+        </button>
+      </div>
+    </div>
+  );
+}
