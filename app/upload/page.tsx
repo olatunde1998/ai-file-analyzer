06c819dc29dba63db8d0b34f327b06e8551dfc1e
geodevcode <geodevinstitute@gmail.com>
2025-07-19 11:50:35 +0100

    metadata added to UploadHome page

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,4 +1,10 @@
 import Upload from "@/components/Upload";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Resumind | Upload",
+  description: "Smart feedback for your dream job!",
+};
 
 export default function UploadHome() {
   return (
