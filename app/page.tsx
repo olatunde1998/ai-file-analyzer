194cda969f4fbcb62a6e1255d06e017ca742031c
geodevcode <geodevinstitute@gmail.com>
2025-07-19 09:04:27 +0100

    HomePage updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { FileUpload } from "@/components/file-upload";
+// import { FileUpload } from "@/components/file-upload";
+import Home from "@/components/Home";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="container mx-auto py-10 px-4 md:px-6 flex flex-col items-center">
-      <div className="max-w-3xl mx-auto space-y-6">
+    <main className="">
+      <Home />
+      {/* <div className="max-w-3xl mx-auto space-y-6">
         <div className="space-y-2">
           <h1 className="text-3xl font-bold">AI File Analyzer</h1>
           <p className="text-muted-foreground">
@@ -11,7 +13,7 @@ export default function Home() {
           </p>
         </div>
         <FileUpload maxFiles={5} maxSize={5} accept="image/*,application/pdf" />
-      </div>
+      </div> */}
     </main>
   );
 }
